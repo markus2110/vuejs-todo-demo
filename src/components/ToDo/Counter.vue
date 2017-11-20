1aab379850c6621580708442473a8ebdca88aa04
Markus <markus@simpleasthat.de>
2017-11-20 23:45:01 +0100

    filter added

--- src/components/ToDo/Counter.vue
+++ src/components/ToDo/Counter.vue
@@ -2,22 +2,34 @@
     
     <div class="right menu">
         
-            <div class="item" title="todos closed">
-                <i class='checkmark icon green'></i>
-                <span class="red">{{todos.filter(todo => {return todo.done === true}).length}}</span>
-            </div>
-            
-            <div class="item" title="todos open">
-                <i class='wait icon orange'></i>
-                {{todos.filter(todo => {return todo.done === false}).length}}
-            </div>
-            
+        <div class="item" title="todos closed">
+            <i class='checkmark icon green'></i>
+            <span>{{ todos | closedCount }}</span>
+        </div>
 
+        <div class="item">
+            <i class='wait icon orange'></i>
+            <span>{{ todos | openCount }}</span>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         props: ['todos'],
+        
+        filters: {
+            closedCount : function(value){
+                var closed = value.filter(todo => {return todo.done === true});
+                return closed.length;
+            },
+            
+            openCount : function(value){
+                var open = value.filter(todo => {return todo.done === false});
+                return open.length;                
+            }          
+
+        }
+        
     }
 </script>
